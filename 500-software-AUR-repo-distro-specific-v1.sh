80de83c44d1dd10152aa7aad5359d9e378837143
deadsidestar <sxycoup75@protonmail.com>
2018-04-17 19:53:11 -0700

    added scripts

---
+++ 500-software-AUR-repo-distro-specific-v1.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+##################################################################################################################
+# Author 	: 	Erik Dubois
+# Website : https://www.erikdubois.be
+# Website	:	https://www.arcolinux.info
+# Website	:	https://www.arcolinux.com
+# Website	:	https://www.arcolinuxd.com
+# Website	:	https://www.arcolinuxforum.com
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+echo "AUR - DESKTOP SPECIFIC APPLICATIONS "
+
+sh AUR-DS/install-gtk2-perl-v*.sh
+
+sh AUR-DS/install-perl-linux-desktopfiles-v*.sh
+
+sh AUR-DS/install-xfce-slimlock-v*.sh
+
+sh AUR-DS/install-sutils-git-v*.sh
+
+sh AUR-DS/install-xtitle-v*.sh
+
+sh AUR-DS/install-python-pywal-v*.sh
+
+echo "################################################################"
+echo "####       Software from AUR-DS folder installed          ######"
+echo "################################################################"
---
+++ 920-fix-oblogout-icons-v1.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+##################################################################################################################
+# Author 	: 	Erik Dubois
+# Website : https://www.erikdubois.be
+# Website	:	https://www.arcolinux.info
+# Website	:	https://www.arcolinux.com
+# Website	:	https://www.arcolinuxd.com
+# Website	:	https://www.arcolinuxforum.com
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+echo "Sardi icons applied when logging out or shutdown or ..."
+
+sudo sed -i 's/buttontheme = oxygen/buttontheme = Sardi-Oblogout/g' /etc/oblogout.conf
+#sudo sed -i 's/'buttontheme = oxygen'/'buttontheme = adeos-branco'/g' /etc/oblogout.conf
+#sudo sed -i 's/'buttontheme = oxygen'/'buttontheme = adeos-branco-mono'/g' /etc/oblogout.conf
+#sudo sed -i 's/'buttontheme = oxygen'/'buttontheme = adeos-branco-cores'/g' /etc/oblogout.conf
+
+echo "changing the application to lock your system"
+sudo sed -i 's/lock = xtrlock &/lock = slimlock/g' /etc/oblogout.conf
+sudo sed -i 's/logout = openbox --exit/logout = pkill bspwm/g' /etc/oblogout.conf
+
+echo "################################################################"
+echo "####                  ICONS ARE NOW APPLIED               ######"
+echo "################################################################"
