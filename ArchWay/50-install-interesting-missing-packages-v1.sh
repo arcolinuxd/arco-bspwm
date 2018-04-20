405b31c8a0a2ccd73ed426923bc22f5c4a259c41
Erik Dubois <erik.dubois@gmail.com>
2018-04-20 19:10:11 +0200

    extra script for missing elements on arch

---
+++ ArchWay/50-install-interesting-missing-packages-v1.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#set -e
+##################################################################################################################
+# Author	:	Nick Petrov
+# Author	:	Kevin Yanez
+# Author	:	Erik Dubois
+# Website	:	https://www.erikdubois.be
+# Website	:	https://www.arcolinux.info
+# Website	:	https://www.arcolinux.com
+# Website	:	https://www.arcolinuxd.com
+# Website	:	https://www.arcolinuxforum.com
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+sudo pacman -S network-manager-applet --noconfirm --needed
+
+echo "################################################################"
+echo "###                  archlinuxfr hashtagged out             ####"
+echo "################################################################"
