5fcd9b3fe4f48064547fdd89fcd9117330c9b06f
Erik Dubois <erik.dubois@gmail.com>
2018-05-30 12:39:26 +0200

    100-400

--- 400-software-arch-linux-repo-distro-specific-v1.sh
+++ 400-software-arch-linux-repo-distro-specific-v1.sh
@@ -51,12 +51,10 @@ sudo pacman -S imagemagick --noconfirm --needed
 sudo pacman -S lxappearance-gtk3 --noconfirm --needed
 sudo pacman -S lxrandr --noconfirm --needed
 sudo pacman -S nitrogen --noconfirm --needed
-sudo pacman -S oblogout --noconfirm --needed
 sudo pacman -S playerctl --noconfirm --needed
 sudo pacman -S qt4 --noconfirm --needed
 sudo pacman -S rofi --noconfirm --needed
 sudo pacman -S thunar --noconfirm --needed
-sudo pacman -S slim --noconfirm --needed
 sudo pacman -S w3m  --noconfirm --needed
 sudo pacman -S xdo  --noconfirm --needed
 sudo pacman -S xdotool  --noconfirm --needed
