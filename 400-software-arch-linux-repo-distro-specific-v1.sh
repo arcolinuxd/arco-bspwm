a2a1ffabd340ebb17ce49f5a2cb25af1202e9368
Erik Dubois <erik.dubois@gmail.com>
2018-12-26 21:24:29 +0100

    added qt5ct to all desktops

--- 400-software-arch-linux-repo-distro-specific-v1.sh
+++ 400-software-arch-linux-repo-distro-specific-v1.sh
@@ -52,7 +52,6 @@ sudo pacman -S lxappearance-gtk3 --noconfirm --needed
 sudo pacman -S lxrandr --noconfirm --needed
 sudo pacman -S nitrogen --noconfirm --needed
 sudo pacman -S playerctl --noconfirm --needed
-sudo pacman -S qt4 --noconfirm --needed
 sudo pacman -S rofi --noconfirm --needed
 sudo pacman -S thunar --noconfirm --needed
 #sudo pacman -S volumeicon  --noconfirm --needed
