7b516bb78d47b8baf5d021f3169876029dae25c0
Erik Dubois <erik.dubois@gmail.com>
2018-12-28 09:26:48 +0100

    xorg-xrandr out - is in group package xorg-apps

--- 400-software-arch-linux-repo-distro-specific-v1.sh
+++ 400-software-arch-linux-repo-distro-specific-v1.sh
@@ -58,7 +58,6 @@ sudo pacman -S thunar --noconfirm --needed
 sudo pacman -S w3m  --noconfirm --needed
 sudo pacman -S xdo  --noconfirm --needed
 sudo pacman -S xdotool  --noconfirm --needed
-sudo pacman -S xorg-xrandr --noconfirm --needed
 sudo pacman -S xfce4-appfinder --noconfirm --needed
 sudo pacman -S xfce4-power-manager --noconfirm --needed
 sudo pacman -S xfce4-screenshooter --noconfirm --needed
